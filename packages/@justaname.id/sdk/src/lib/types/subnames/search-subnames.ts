faef148f9699e06b0b3956c6fc6977f5169fe10b
anthony23991 <anthonykhoury23991@hotmail.com>
2024-04-22 19:16:49 +0300

    feat: added invitations, communitySubnames and searchSubnames hooks (#11)

---
+++ packages/@justaname.id/sdk/src/lib/types/subnames/search-subnames.ts
@@ -0,0 +1,71 @@
+import { ChainId, IRequest, IResponse, IRoute } from '../common';
+
+interface AddressResponse {
+  id: string;
+
+  coinType: number;
+
+  address: string;
+
+  dataId: string;
+}
+
+interface TextRecordResponse {
+  id: string;
+
+  key: string;
+
+  value: string;
+
+  dataId: string;
+}
+
+interface MetadataResponse {
+  id: string;
+
+  contentHash: string;
+
+  subdomainId: string;
+
+  addresses: AddressResponse[];
+
+  textRecords: TextRecordResponse[];
+}
+
+export interface SubnameSearchRequest extends IRequest {
+  subname: string;
+
+  skip: number;
+
+  take: number;
+
+  chainId: ChainId;
+
+  data: boolean;
+
+  ensRegistered: boolean;
+
+  isClaimed: boolean;
+}
+
+export interface SubnameSearchResponse extends IResponse {
+  id: string;
+
+  username: string;
+
+  ensId: string;
+
+  subname: string;
+
+  isClaimed: boolean;
+
+  claimedAt?: Date | null;
+
+  data: MetadataResponse;
+}
+
+export interface SubnameSearchRoute extends IRoute {
+  request: SubnameSearchRequest;
+  response: SubnameSearchResponse[];
+  headers: NonNullable<unknown>;
+}
