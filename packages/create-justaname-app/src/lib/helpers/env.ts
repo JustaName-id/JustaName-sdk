f851a21a90c55bc4cce527ae801d92928320abbd
HadiKhai <hadikhairallah@outlook.com>
2024-06-04 19:59:46 +0300

    feat: nextjs updated env in create-justaname-app

--- packages/create-justaname-app/src/lib/helpers/env.ts
+++ packages/create-justaname-app/src/lib/helpers/env.ts
@@ -7,61 +7,63 @@ import { AppDetails, BackendFramework, FrontendFramework, FullstackFramework } f
  */
 export const frameworkEnv: Record<FrontendFramework | BackendFramework | FullstackFramework, string>= {
   'nextjs':
-    `NEXT_PUBLIC_ENS_DOMAIN={{ensDomain}}
-    NEXT_PUBLIC_CHAIN_ID={{chainId}}
-    JUSTANAME_DOMAIN={{domain}}
-    JUSTANAME_CHAIN_ID={{chainId}}
-    JUSTANAME_API_KEY={{apiKey}}
-    JUSTANAME_ORIGIN={{origin}}`,
+`NEXT_PUBLIC_ENS_DOMAIN={{ensDomain}}
+NEXT_PUBLIC_CHAIN_ID={{chainId}}
+JUSTANAME_DOMAIN={{domain}}
+JUSTANAME_CHAIN_ID={{chainId}}
+JUSTANAME_API_KEY={{apiKey}}
+JUSTANAME_ORIGIN={{origin}}
+JUSTANAME_ENS_DOMAIN={{ensDomain}}
+`,
   'react': `
-    VITE_APP_API_URL=http://localhost:3333
-    VITE_APP_ENS_DOMAIN={{ensDomain}}
-    VITE_APP_CHAIN_ID={{chainId}}
+VITE_APP_API_URL=http://localhost:3333
+VITE_APP_ENS_DOMAIN={{ensDomain}}
+VITE_APP_CHAIN_ID={{chainId}}
   `,
   'react-native': `
-    API_URL=http://localhost:3333
-    ENS_DOMAIN={{ensDomain}}
-    CHAIN_ID={{chainId}}
+API_URL=http://localhost:3333
+ENS_DOMAIN={{ensDomain}}
+CHAIN_ID={{chainId}}
   `,
   'expo': `
-    API_URL=http://localhost:3333
-    ENS_DOMAIN={{ensDomain}}
-    CHAIN_ID={{chainId}}
+API_URL=http://localhost:3333
+ENS_DOMAIN={{ensDomain}}
+CHAIN_ID={{chainId}}
   `,
   'express':
-    `JUSTANAME_ENS_DOMAIN={{ensDomain}}
-     JUSTANAME_DOMAIN={{domain}}
-     JUSTANAME_CHAIN_ID={{chainId}}
-     JUSTANAME_API_KEY={{apiKey}}
-     JUSTANAME_ORIGIN={{origin}}
-    `,
+`JUSTANAME_ENS_DOMAIN={{ensDomain}}
+ JUSTANAME_DOMAIN={{domain}}
+ JUSTANAME_CHAIN_ID={{chainId}}
+ JUSTANAME_API_KEY={{apiKey}}
+ JUSTANAME_ORIGIN={{origin}}
+`,
   'nestjs': `
-    JUSTANAME_ENS_DOMAIN={{ensDomain}}
-    JUSTANAME_DOMAIN={{domain}}
-    JUSTANAME_CHAIN_ID={{chainId}}
-    JUSTANAME_API_KEY={{apiKey}}
-    JUSTANAME_ORIGIN={{origin}}
+JUSTANAME_ENS_DOMAIN={{ensDomain}}
+JUSTANAME_DOMAIN={{domain}}
+JUSTANAME_CHAIN_ID={{chainId}}
+JUSTANAME_API_KEY={{apiKey}}
+JUSTANAME_ORIGIN={{origin}}
   `,
   'koa': `
-    JUSTANAME_ENS_DOMAIN={{ensDomain}}
-    JUSTANAME_DOMAIN={{domain}}
-    JUSTANAME_CHAIN_ID={{chainId}}
-    JUSTANAME_API_KEY={{apiKey}}
-    JUSTANAME_ORIGIN={{origin}}
+JUSTANAME_ENS_DOMAIN={{ensDomain}}
+JUSTANAME_DOMAIN={{domain}}
+JUSTANAME_CHAIN_ID={{chainId}}
+JUSTANAME_API_KEY={{apiKey}}
+JUSTANAME_ORIGIN={{origin}}
   `,
   'hapi': `
-    JUSTANAME_ENS_DOMAIN={{ensDomain}}
-    JUSTANAME_DOMAIN={{domain}}
-    JUSTANAME_CHAIN_ID={{chainId}}
-    JUSTANAME_API_KEY={{apiKey}}
-    JUSTANAME_ORIGIN={{origin}}
+JUSTANAME_ENS_DOMAIN={{ensDomain}}
+JUSTANAME_DOMAIN={{domain}}
+JUSTANAME_CHAIN_ID={{chainId}}
+JUSTANAME_API_KEY={{apiKey}}
+JUSTANAME_ORIGIN={{origin}}
   `,
   'fastify': `
-    JUSTANAME_ENS_DOMAIN={{ensDomain}}
-    JUSTANAME_DOMAIN={{domain}}
-    JUSTANAME_CHAIN_ID={{chainId}}
-    JUSTANAME_API_KEY={{apiKey}}
-    JUSTANAME_ORIGIN={{origin}}
+JUSTANAME_ENS_DOMAIN={{ensDomain}}
+JUSTANAME_DOMAIN={{domain}}
+JUSTANAME_CHAIN_ID={{chainId}}
+JUSTANAME_API_KEY={{apiKey}}
+JUSTANAME_ORIGIN={{origin}}
   `
 }
 
@@ -75,10 +77,10 @@ export const frameworkEnv: Record<FrontendFramework | BackendFramework | Fullsta
 export const getEnv = (framework: FrontendFramework | BackendFramework | FullstackFramework, appDetails: AppDetails) => {
   const env = frameworkEnv[framework];
   const envMatched =  env.replace(/{{ensDomain}}/g, appDetails.ensDomain)
-            .replace(/{{chainId}}/g, appDetails.network === 'mainnet' ? '1' : '11155111')
-            .replace(/{{apiKey}}/g, appDetails.apiKey)
-            .replace(/{{origin}}/g, appDetails.appUrl)
-            .replace(/{{domain}}/g, appDetails.appUrl.split('://')[1])
+      .replace(/{{chainId}}/g, appDetails.network === 'mainnet' ? '1' : '11155111')
+      .replace(/{{apiKey}}/g, appDetails.apiKey)
+      .replace(/{{origin}}/g, appDetails.appUrl)
+      .replace(/{{domain}}/g, appDetails.appUrl.split('://')[1])
 
   return envMatched;
 }
