67043376e0300a1b395f0db82a2cdda10271b3fe
HadiKhai <hadikhairallah@outlook.com>
2024-10-31 17:21:40 +0200

    fix: error from useEnsWalletClient

--- packages/@justaname.id/react/src/lib/hooks/client/useEnsWalletClient.ts
+++ packages/@justaname.id/react/src/lib/hooks/client/useEnsWalletClient.ts
@@ -1,31 +1,50 @@
 import { useWalletClient } from 'wagmi';
-import { WalletClient, Transport, Account, Chain, RpcSchema } from 'viem';
+import { Account, Chain, RpcSchema, Transport, WalletClient } from 'viem';
 import { addEnsContracts } from '@ensdomains/ensjs';
 import { CheckedChainWithEns } from '@ensdomains/ensjs/contracts';
 
 export interface UseEnsWalletClientResult {
-  ensWalletClient: WalletClient<Transport, CheckedChainWithEns<Chain>, Account, RpcSchema> | undefined
-  isEnsWalletClientPending: boolean
+  ensWalletClient:
+    | WalletClient<Transport, CheckedChainWithEns<Chain>, Account, RpcSchema>
+    | undefined;
+  isEnsWalletClientPending: boolean;
 }
 
 export interface UseEnsWalletClientParams {}
 
-export const useEnsWalletClient = (params?: UseEnsWalletClientParams): UseEnsWalletClientResult => {
-  const query = useWalletClient()
+export const useEnsWalletClient = (
+  params?: UseEnsWalletClientParams
+): UseEnsWalletClientResult => {
+  const query = useWalletClient();
 
-  const transformWalletToEnsWallet = (wallet: WalletClient<Transport, Chain, Account, RpcSchema> | undefined): WalletClient<Transport, CheckedChainWithEns<Chain>, Account, RpcSchema> | undefined => {
+  const transformWalletToEnsWallet = (
+    wallet: WalletClient<Transport, Chain, Account, RpcSchema> | undefined
+  ):
+    | WalletClient<Transport, CheckedChainWithEns<Chain>, Account, RpcSchema>
+    | undefined => {
     if (!wallet) {
-      return
+      return;
+    }
+
+    if (wallet?.chain.id !== 1 && wallet?.chain.id !== 11155111) {
+      return;
     }
 
     return {
       ...wallet,
-      chain: addEnsContracts(wallet.chain as Chain)
-    } as WalletClient<Transport, CheckedChainWithEns<Chain>, Account, RpcSchema>
-  }
+      chain: addEnsContracts(wallet.chain as Chain),
+    } as WalletClient<
+      Transport,
+      CheckedChainWithEns<Chain>,
+      Account,
+      RpcSchema
+    >;
+  };
 
   return {
-    ensWalletClient:  transformWalletToEnsWallet(query.data as WalletClient<Transport, Chain, Account, RpcSchema>),
-    isEnsWalletClientPending: query.isPending
-  }
-}
+    ensWalletClient: transformWalletToEnsWallet(
+      query.data as WalletClient<Transport, Chain, Account, RpcSchema>
+    ),
+    isEnsWalletClientPending: query.isPending,
+  };
+};
