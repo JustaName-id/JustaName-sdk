6fb29aad1ff7fda0a174c7d86d25e19dd608a9c4
HadiKhai <hadikhairallah@outlook.com>
2024-10-31 11:43:43 +0200

    trial

--- packages/@justaname.id/react/update-package-json.js
+++ packages/@justaname.id/react/update-package-json.js
@@ -26,16 +26,16 @@ if (packageJson.exports) {
   });
 }
 
-// Write the modified package.json to the dist folder
-console.log('Writing package.json to dist folder');
-// I want to see the content of __dirname
-fs.readdirSync(__dirname).forEach((file) => {
-  console.log(file);
-});
-
-fs.readdirSync(path.join(__dirname, 'dist')).forEach((file) => {
-  console.log(file);
-});
+// // Write the modified package.json to the dist folder
+// console.log('Writing package.json to dist folder');
+// // I want to see the content of __dirname
+// fs.readdirSync(__dirname).forEach((file) => {
+//   console.log(file);
+// });
+//
+// fs.readdirSync(path.join(__dirname, 'dist')).forEach((file) => {
+//   console.log(file);
+// });
 
 const distPath = path.join(__dirname, 'dist', 'package.json');
 fs.writeFileSync(distPath, JSON.stringify(packageJson, null, 2));
