e384c09e3c6de53ebb92147992de24920980da09
HadiKhai <hadikhairallah@outlook.com>
2024-10-31 11:58:28 +0200

    trial

--- apps/console/next.config.js
+++ apps/console/next.config.js
@@ -7,7 +7,7 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  distDir: '../../dist/apps/console',
+  // distDir: '../../dist/apps/console',
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
