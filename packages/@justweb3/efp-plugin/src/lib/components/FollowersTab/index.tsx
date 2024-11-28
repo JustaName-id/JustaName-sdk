aa71418bf23cf9b99a17bcf84d6304f72ca5a64c
JustHadi <35467011+HadiKhai@users.noreply.github.com>
2024-11-28 19:01:08 +0200

    Hadi/fix efp poap (#77)
    * feat: basic but need to move to js sdk
    
    * feat: chat and getting the messages
    
    * fix: tabs css
    
    * feat: working on chat
    
    * feat: finalizing xmtp widget
    
    * fix: message textfield fix when replying
    
    * fix: xmtp build
    
    * fix: efp poap
    
    ---------
    
    Co-authored-by: anthony2399 <anthonykhoury23991@hotmail.com>

--- packages/@justweb3/efp-plugin/src/lib/components/FollowersTab/index.tsx
+++ packages/@justweb3/efp-plugin/src/lib/components/FollowersTab/index.tsx
@@ -9,8 +9,9 @@ import {
 
 export interface FollowersTabProps {
   ens: string;
+  address: string;
 }
-export const FollowersTab: React.FC<FollowersTabProps> = ({ ens }) => {
+export const FollowersTab: React.FC<FollowersTabProps> = ({ ens, address }) => {
   const [nbOfCardsPerFollowersRow, setNbOfCardsPerFollowersRow] = useState(0);
   const followingContainerRef = useRef<HTMLDivElement>(null);
   const {
@@ -20,7 +21,7 @@ export const FollowersTab: React.FC<FollowersTabProps> = ({ ens }) => {
     isFollowersFetching,
     hasMoreFollowers,
   } = useFollowers({
-    addressOrEns: ens,
+    addressOrEns: address,
   });
 
   const followingFlat = useMemo(() => {
@@ -95,7 +96,7 @@ export const FollowersTab: React.FC<FollowersTabProps> = ({ ens }) => {
     >
       {followingFlat?.map((follow) => (
         <div
-          key={`display-following-members-${ens}-${follow.address}`}
+          key={`display-following-members-${ens}-${address}-${follow.address}`}
           style={{
             minWidth: '335px',
             flex: 1,
@@ -138,7 +139,7 @@ export const FollowersTab: React.FC<FollowersTabProps> = ({ ens }) => {
           },
           (_, i) => (
             <div
-              key={`display-following-members-${ens}-${i}`}
+              key={`display-following-members-${ens}-${address}-${i}`}
               style={{
                 minWidth: '335px',
                 flex: 1,
