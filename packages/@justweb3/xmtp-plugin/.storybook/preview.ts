fd63310fe5508339306c4be21d3480fc0fb6be0e
anthony2399 <anthonykhoury23991@hotmail.com>
2025-02-07 16:29:57 +0200

    fix: pkgs

---
+++ packages/@justweb3/xmtp-plugin/.storybook/preview.ts
@@ -0,0 +1,21 @@
+import type { Preview } from '@storybook/react';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    server: {
+      headers: {
+        'Cross-Origin-Embedder-Policy': 'require-corp',
+        'Cross-Origin-Opener-Policy': 'same-origin',
+      },
+    },
+  },
+};
+
+export default preview;
