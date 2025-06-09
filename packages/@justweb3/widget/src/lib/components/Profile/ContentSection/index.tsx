940864a18ae67eeaa2351a9fe3978e9e3d0d2c1c
anthony2399 <anthonykhoury23991@hotmail.com>
2025-06-09 13:39:21 +0300

    feat(widget): release

--- packages/@justweb3/widget/src/lib/components/Profile/ContentSection/index.tsx
+++ packages/@justweb3/widget/src/lib/components/Profile/ContentSection/index.tsx
@@ -51,8 +51,9 @@ export interface ContentProps {
   plugins: JustaPlugin[];
 }
 
-// const ENS_MAINNET_RESOLVER = ['0x4976fb03C32e5B8cfe2b6cCB31c09Ba78EBaBa41', '0x231b0Ee14048e9dCcD1d247744d114a4EB5E8E63']
+// const ENS_MAINNET_RESOLVER = ['0x4976fb03C32e5B8cfe2b6cCB31c09Ba78EBaBa41', '0x231b0Ee14048e9dCcD1d247744d114a4EB5E8E63'];
 // const ENS_SEPOLIA_RESOLVER = '0x8FADE66B79cC9f707aB26799354482EB93a5B7dD';
+
 
 const ContentSection: React.FC<ContentProps> = ({
   fullSubname = '',
