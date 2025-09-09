ce37a3c0697b6e3b70d40572d654968cc330b854
Ghadi Mhawej <ghadimhawej@outlook.com>
2025-09-09 10:58:32 +0300

    feat: deprecate ethers v5 support

--- packages/@justaname.id/siwens/src/lib/utils/ethersCompat.ts
+++ packages/@justaname.id/siwens/src/lib/utils/ethersCompat.ts
@@ -1,51 +1,17 @@
-// inspired by spruceid siwe: https://github.com/spruceid/siwe/blob/main/packages/siwe/lib/ethersCompat.ts
+// Ethers v6 compatibility layer
 
 import { ethers } from 'ethers';
 
-// @ts-expect-error -- compatibility hack
-type ProviderV5 = ethers.providers.Provider;
-type ProviderV6 = ethers.Provider;
-// @ts-expect-error -- compatibility hack
-type JsonRpcProviderV5 = ethers.providers.JsonRpcProvider;
-type JsonRpcProviderV6 = ethers.JsonRpcProvider;
-
-export type Provider = ProviderV6 extends undefined ? ProviderV5 : ProviderV6;
-export type JsonRpcProvider = JsonRpcProviderV6 extends undefined
-  ? JsonRpcProviderV5
-  : JsonRpcProviderV6;
-
-interface EthersCompat {
-  namehash?: (name: string) => string;
-  getAddress?: (address: string) => string;
-  JsonRpcProvider?: new (...args: any[]) => JsonRpcProvider;
-  utils: {
-    namehash: (name: string) => string;
-    getAddress: (address: string) => string;
-  };
-  providers: {
-    JsonRpcProvider: new (...args: any[]) => JsonRpcProvider;
-  };
-}
-
-const ethersCompat = ethers as unknown as EthersCompat;
+export type Provider = ethers.Provider;
+export type JsonRpcProvider = ethers.JsonRpcProvider;
 
 export const getJsonRpcProvider = (
   providerUrl?: string,
   chainId?: number
 ): JsonRpcProvider => {
-  if ('JsonRpcProvider' in ethersCompat) {
-    return new ethersCompat.JsonRpcProvider!(providerUrl, chainId);
-  } else {
-    return new ethersCompat.providers.JsonRpcProvider(providerUrl, chainId);
-  }
+  return new ethers.JsonRpcProvider(providerUrl, chainId);
 };
 
-export const namehash: (name: string) => string =
-  'namehash' in ethersCompat
-    ? ethersCompat.namehash!
-    : ethersCompat.utils.namehash;
+export const namehash = (name: string): string => ethers.namehash(name);
 
-export const getAddress: (address: string) => string =
-  'getAddress' in ethersCompat
-    ? ethersCompat.getAddress!
-    : ethersCompat.utils.getAddress;
+export const getAddress = (address: string): string => ethers.getAddress(address);
